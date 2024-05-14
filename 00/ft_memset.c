1f6742dcf41f9e00b6e6868c773f1754337b84c0
itben-h <67160296+itben-h@users.noreply.github.com>
2024-05-14 20:41:25 +0800

    Working files

---
+++ 00/ft_memset.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bhowe <bhowe@student.42singapore.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/14 12:29:17 by bhowe             #+#    #+#             */
+/*   Updated: 2024/05/14 13:02:28 by bhowe            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*str;
+
+	str = (unsigned char *)s;
+	while (n--)
+		*str++ = (unsigned char)c;
+	return (s);
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# tester
+
+### For testing: 
+
+- Place the 'tests' folder into the required project folder
+- Run the Makefile with the command 'test'
+- Works??
