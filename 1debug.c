2ab6b2f40d8137494ef5f67872166057fa1b900f
itben-h <67160296+itben-h@users.noreply.github.com>
2024-05-28 12:23:39 +0800

    use this as wip

--- 1debug.c
+++ 1debug.c
@@ -7,40 +7,28 @@
 #include <fcntl.h>
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 5
+# define BUFFER_SIZE 100
 #endif
 
-size_t	ft_strlen(char *s)
+size_t	findi(char *s, char c)
 {
 	size_t	i;
 
 	i = 0;
-	while (s[i])
+	while (s[i] != c && s[i])
 		i++;
 	return (i);
 }
 
-size_t	check_nl(char *buf)
-{
-	size_t	i;
-
-	i = 0;
-	while(buf[i])
-	{
-		if (buf[i] == '\n')
-			break ;
-		i++;
-	}
-	return (i);
-}
-
-char	*trim_before_nl(char *buf, size_t n)
+char	*trim_b_nl(char *buf, size_t n)
 {
 	char	*s;
 	size_t	i;
 	size_t	j;
-	
-	s = (char *)malloc(ft_strlen(buf + n) * sizeof(char));
+	size_t	len;
+
+	len = findi(buf + n, '\0');
+	s = (char *)malloc(len * sizeof(char));
 	if (!s)
 		return (0);
 	i = n + 1;
@@ -55,7 +43,7 @@ char	*trim_before_nl(char *buf, size_t n)
 	return (s);
 }
 
-char	*trim_after_nl(char *buf, size_t n)
+char	*trim_a_nl(char *buf, size_t n)
 {
 	char	*s;
 	size_t	i;
@@ -82,7 +70,7 @@ char	*ft_strjoin(char *s1, char *s2)
 
 	i = -1;
 	j = -1;
-	len_total = ft_strlen(s1) + ft_strlen(s2);
+	len_total = findi(s1, '\0') + findi(s1, '\0');
 	new = (char *)malloc((len_total + 1) * sizeof(char));
 	if (!new)
 		return (0);
@@ -91,43 +79,50 @@ char	*ft_strjoin(char *s1, char *s2)
 	while (s2[++j])
 		new[i + j] = s2[j];
 	new[i + j] = '\0';
-	free(s1);
 	return (new);
 }
 
-char	*alloc_buf(char* line, char *buf)
+char	*alloc_mem(char *buf, char *mem)
 {
-	static char	*mem;
+	char	*add;
+	
+	add = trim_b_nl(buf, findi(buf, '\n') + 1);
+	mem = ft_strjoin(mem, add);
+	return (mem);
+}
 
-	if (mem && !line)
-		line = ft_strjoin(line, mem);
-	if (check_nl(buf) != ft_strlen(buf))
+char	*alloc_line(char* line, char *buf, char *mem)
+{
+	if (mem && !line[0])
 	{
-		mem = trim_before_nl(buf, check_nl(buf));
-		buf = trim_after_nl(buf, check_nl(buf));
+		if (findi(mem, '\n') != findi(mem, '\0'))
+			line = ft_strjoin(line, trim_a_nl(mem, findi(mem, '\n')));
+		else
+			line = ft_strjoin(line, mem);
 	}
-	else
-		mem = buf;
-	line = ft_strjoin(line, buf);
+	if (findi(buf, '\n') != findi(buf, '\0'))
+		buf = trim_a_nl(buf, findi(buf, '\n'));
+	if (buf)
+		line = ft_strjoin(line, buf);
 	return (line);
 }
 
 char	*get_next_line(int fd)
 {
-	char	*buf;
-	char	*line;
-	size_t	count;
+	char		*line;
+	static char	*buf;
 
-	count = BUFFER_SIZE;
 	line = (char *)malloc(sizeof(char));
 	line[0] = '\0';
 	buf = (char *)malloc(count * (sizeof(char) + 1));
 	if (!buf)
 		return (0);
-	while (read(fd, buf, count))
+	buf[count] = '\0';
+	while (read(fd, buf, count) || mem)
 	{
-		line = alloc_buf(line, buf);
-		if (line[ft_strlen(line) - 1] == '\n')
+		line = alloc_line(line, buf, mem);
+		mem = alloc_mem(buf, mem);
+		if (line[findi(line, '\0') - 1] == '\n')
 			break ;
 	}
 	free(buf);
@@ -138,7 +133,5 @@ int main()
 {
 	int fd = open("./test1", O_RDWR);
 	char *s = get_next_line(fd);
-	printf("%s", s);
 	s = get_next_line(fd);
-	printf("%s", s);
 }
